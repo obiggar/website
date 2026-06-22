c88c594ec24cd1511137ffcbbd8e415e0cc84ae0
obiggar <43942670+obiggar@users.noreply.github.com>
2026-06-22 19:16:20 +0000

    Deploying to gh-pages from @ obiggar/obiggar.github.io@011fcb94265b9f3562c104caed53232f20dc6db1 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "obiggar/website",
+    "data-repo": "obiggar/obiggar.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
